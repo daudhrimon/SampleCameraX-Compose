c3f5121f5279f65227940d0b1124c3ae1cc138c0
Daud Hoshen <daud@sharetrip.net>
2025-04-13 19:08:03 +0600

    Initial commit

---
+++ app/src/main/java/com/app/samplecamerax/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.app.samplecamerax
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import com.app.samplecamerax.composables.CameraPreviewScreen
+import com.app.samplecamerax.ui.theme.CmrXTutorialTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                setCameraPreview()
+            } else {
+                // Camera permission denied
+            }
+
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                setCameraPreview()
+            }
+            else -> {
+                cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+            }
+        }
+
+    }
+    private fun setCameraPreview() {
+        setContent {
+            CmrXTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraPreviewScreen()
+                }
+            }
+        }
+    }
+}
